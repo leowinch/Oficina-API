194c53f293d1c6fe6536d404101854dc7e2633a4
Leonardo Winch Dallanora <139998093+leowinch@users.noreply.github.com>
2025-08-25 19:45:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 app = FastAPI() # cria a aplicação e será o ponto central das rotas
 
 
-@app.get("/products") # Requisição GET com URL "/products/{product_id}" 
-def buscar_produto(): # Função que implementa a requisição
+@app.get("/products") # Requisição GET com URL "/products" 
+def buscar_produtos(): # Função que implementa a requisição
     
     url = f"https://dummyjson.com/products/" # URL da API externa para pegar os dados
     
